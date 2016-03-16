27d90c5ad87d0ac3ba75b2bbf760cf53e086d985
Emil Maran <emil@mycrm.de>
2016-03-16 11:40:36 +0100

    howto

---
+++ php/php_howto.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emil
+ * Date: 16.03.16
+ * Time: 11:36
+ */
+
+// GET PATH ---------------------------------------------
+// basename(__FILE__); // Get File name
+// dirname($_SERVER["SCRIPT_FILENAME"]); // Get Dir name
+// print_r(pathinfo($_SERVER["SCRIPT_FILENAME"])); // dirname / basename
